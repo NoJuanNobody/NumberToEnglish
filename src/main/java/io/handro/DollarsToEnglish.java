4d2d1f8084fda4fdd3a6d904fd47f365e4f4ab50
Alejandro Londono <alondono11@gmail.com>
2016-06-09 12:31:56 -0400

    first commit

---
+++ src/main/java/io/handro/DollarsToEnglish.java
@@ -0,0 +1,13 @@
+package io.handro;
+
+/**
+ * Created by alejandrolondono on 6/9/16.
+ */
+public class DollarsToEnglish {
+    public enum Ones{}
+    public enum Tens{}
+    public enum Decimal{}
+
+
+
+}
---
+++ src/test/java/io/handro/DollarsToEnglishTest.java
@@ -0,0 +1,7 @@
+package io.handro;
+
+/**
+ * Created by alejandrolondono on 6/9/16.
+ */
+public class DollarsToEnglishTest {
+}
